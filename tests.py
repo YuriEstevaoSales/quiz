4aa381b17d6cf3a76f814cdc77a5e8d82bb65c50
Yuri Estevao <notoridake@proton.me>
2025-09-05 23:13:31 -0300

    Commit 2: Creating 10 unit tests

--- tests.py
+++ tests.py
@@ -34,3 +34,90 @@ def test_create_choice():
     assert len(question.choices) == 1
     assert choice.text == 'a'
     assert not choice.is_correct
+
+def test_create_choice_with_invalid_text():
+    with pytest.raises(Exception):
+        Choice(text='')
+    with pytest.raises(Exception):
+        Choice(text='a'*101)
+    with pytest.raises(Exception):
+        Choice(text='a'*500)
+
+def test_remove_choice():
+    question = Question(title='q1')
+
+    question.add_choice('a', False)
+
+    choice = question.choices[0]
+    question.remove_choice_by_id(1)
+    assert len(question.choices) == 0
+
+def test_remove_non_existing_choice():
+    with pytest.raises(Exception):
+        question.remove_choice_by_id(1)
+
+def test_remove_all_questions():
+    question = Question(title='q1')
+
+    question.add_choice('a', False)
+    question.add_choice('b', False)
+
+    assert len(question.choices) == 2 
+    question.remove_all_choices()
+    assert len(question.choices) == 0
+
+def test_correct_choice():
+    question = Question(title='q1')
+
+    question.add_choice('a', False)
+    question.set_correct_choices([1])
+
+    assert question.correct_selected_choices([1]) == [1]
+
+def test_multiple_correct_choices():
+    question = Question(title='q1')
+
+    question.add_choice('a', False)
+    question.add_choice('b', False)
+    question.add_choice('c', False)
+    question.set_correct_choices([2, 3])
+    assert question.correct_selected_choices([2]) == [2]
+    assert question.correct_selected_choices([3]) == [3]
+
+def test_select_more_than_max_selections_allowed():
+    question = Question(title='q1', max_selections=2)
+
+    question.add_choice('a', False)
+    question.add_choice('b', False)
+    question.add_choice('c', False)
+    question.set_correct_choices([2, 3])
+
+    with pytest.raises(Exception):
+        question.correct_selected_choices([1, 2, 3])
+
+def test_correct_wrong_choice():
+    question = Question(title='q1')
+    
+    question.add_choice('a', False)
+    question.add_choice('b', False)
+    question.add_choice('c', False)
+    question.set_correct_choices([2, 3])
+
+    assert question.correct_selected_choices([1]) == []
+
+def test_create_question_with_invalid_points(): 
+    with pytest.raises(Exception):
+        question = Question(title='q1', points=101)
+
+def test_if_generated_choices_ids_are_sequential():
+    question = Question(title='q1')
+
+    question.add_choice('a', False)
+    question.add_choice('b', False)
+    question.add_choice('c', False)
+
+    question.remove_choice_by_id(2)
+
+    question.add_choice('d', False)
+    choice = question.choices[2]
+    assert choice.id == 4
